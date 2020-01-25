3362d0a8c8c779c2220a8c03ddb1aee14057c72b
Erbel, Marcin <Marcin.Erbel@harman.com>
2020-01-25 06:33:11 +0100

    init

---
+++ src/main/java/io/github/javafaktura/s02e08/S02e08Application.java
@@ -0,0 +1,13 @@
+package io.github.javafaktura.s02e08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S02e08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S02e08Application.class, args);
+	}
+
+}
