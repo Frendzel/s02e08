08045dfe43082910e6e2cab07ad099b05969a8db
Erbel, Marcin <Marcin.Erbel@harman.com>
2020-01-31 19:16:32 +0100

    init

---
+++ noboot/s02e08_2/src/main/java/io/github/javafaktura/App.java
@@ -0,0 +1,10 @@
+package io.github.javafaktura;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+    }
+}
